074162576992aaee54258835e6521f97b0e8911b
Ryan McGee <ramtech51@comcast.net>
2016-09-21 22:55:02 -0400

    Finished the first homework assignment

---
+++ src/Homework1.java
@@ -0,0 +1,21 @@
+
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+			System.out.println("A number between 0 and 10: ");
+			System.out.println((int) (Math.random() * 10));
+			System.out.println("A number between 0 and 500: ");
+			System.out.println((int) (Math.random() * 500));
+			System.out.println("A number between 1 and 10: ");
+			System.out.println((int) ((Math.random() * 9) + 1));
+			System.out.println("A number between 1 and 500: ");
+			System.out.println((int) ((Math.random() * 499) + 1));
+			System.out.println("A number between 25 and 50: ");
+			System.out.println((int) ((Math.random() * 25) + 25));
+			System.out.println("A number between -10 and 15: ");
+			System.out.println((int) ((Math.random() * 25) - 10));
+			
+	}
+
+}
