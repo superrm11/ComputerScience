2e72d4e37ab523d05ed7443bae7bf61062970215
mcgeeryana <mcgeeryana@MV0411095262J10.mv.scps.k12.va.us>
2016-09-22 10:18:50 -0400

    Completed another IBCS assignment

---
+++ src/ibcs/FullName.java
@@ -0,0 +1,17 @@
+package ibcs;
+
+import java.util.Scanner;
+
+public class FullName {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your first name? ");
+		String fName = input.next();
+		System.out.println("What is your last name? ");
+		String lName = input.next();
+		System.out.println("Your full name is " + fName + " " + lName + ".");
+
+	}
+
+}
