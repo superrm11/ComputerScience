2934b4b0c14aabf5b159f052f0f3272255f2db85
mcgeeryana <mcgeeryana@MV0411095262J10.mv.scps.k12.va.us>
2016-09-22 10:25:05 -0400

    Finished ibcs project 7.1

---
+++ src/ibcs/RadiusOfCircle.java
@@ -0,0 +1,16 @@
+package ibcs;
+
+import java.util.Scanner;
+
+public class RadiusOfCircle {
+
+	public static void main(String[] args) {
+		System.out.println("Please input the area of the circle: _");
+		Scanner input = new Scanner(System.in);
+		double area = input.nextDouble();
+		double radius = Math.sqrt(area/Math.PI);
+		System.out.println("The radius is: " + radius);
+
+	}
+
+}
