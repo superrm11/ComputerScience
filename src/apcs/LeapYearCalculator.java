38f8db5cb82df257048331081e4d361d392867be
mcgeeryana <mcgeeryana@MV0411095262J10.mv.scps.k12.va.us>
2016-09-26 14:15:41 -0400

    completed more assignments

--- src/apcs/LeapYearCalculator.java
+++ src/apcs/LeapYearCalculator.java
@@ -1,10 +1,28 @@
 package apcs;
 
+import java.util.Scanner;
+
 public class LeapYearCalculator {
 
-	public static void main(String[] args) {
-		
-
+	/**
+	 * @author Ryan McGee
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter a year: ");
+		int year = input.nextInt();
+		if(year < 1582){
+			input.close();
+			throw new Exception();
+		}
+		if((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
+			System.out.println(year + " IS a leap year.");
+		}else{
+			System.out.println(year + " is NOT a leap year.");
+		}
+		input.close();
 	}
 
 }
---
+++ src/apcs/LeapYearCalculator_Looped.java
@@ -0,0 +1,40 @@
+package apcs;
+
+import java.util.Scanner;
+
+public class LeapYearCalculator_Looped {
+
+	/**
+	 * @author Ryan McGee
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		Scanner input = new Scanner(System.in);
+		String command;
+		int year;
+		System.out.print("Please enter a year: ");
+		while (true) {
+			System.out.print("$>");
+			command = input.nextLine();
+			if(command.equals("exit")){
+				break;
+			}else{
+				year = Integer.parseInt(command);
+			}
+			if (year < 1582) {
+				input.close();
+				throw new Exception();
+			}
+			if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) {
+				System.out.println(year + " IS a leap year.");
+			} else {
+				System.out.println(year + " is NOT a leap year.");
+			}
+			
+			System.out.println("Enter another year or type 'exit' to exit.");
+		}
+		input.close();
+	}
+
+}
