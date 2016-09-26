4ea78a5f03fda9bae75dbc6c4b89baf06469692e
mcgeeryana <mcgeeryana@MV0411095262J10.mv.scps.k12.va.us>
2016-09-26 10:28:09 -0400

    Finished 2 more assignments for IB Computer Science

---
+++ src/ibcs/EvenOrOdd.java
@@ -0,0 +1,21 @@
+package ibcs;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+	/**
+	 * @author Ryan McGee
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please input an Integer: ");
+		int i = input.nextInt();
+		if(i % 2 == 0){
+			System.out.println(i + " is even.");
+		}else{
+			System.out.println(i + " is odd.");
+		}
+	}
+
+}
---
+++ src/ibcs/PlanetWeight.java
@@ -0,0 +1,48 @@
+package ibcs;
+
+import java.util.Scanner;
+
+public class PlanetWeight {
+	/**
+	 * @author Ryan McGee
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[] planets = { "Voltar", "Krypton", "Fertos", "Servontos" };
+		Scanner input = new Scanner(System.in);
+		boolean finished = false;
+		int selection = 0;
+		
+		System.out.println("What is your weight on earth? ");
+		double weight = input.nextDouble();
+		
+		while (!finished) {
+			System.out.println("\nPlease select the planet: \n1: Voltar\n2: Krypton\n3: Fertos\n4: Servontos\n");
+			selection = input.nextInt();
+			
+			switch (selection) {
+			case 1:
+				weight *= 0.091;
+				finished = true;
+				break;
+			case 2:
+				weight *= 0.720;
+				finished = true;
+				break;
+			case 3:
+				weight *= 0.865;
+				finished = true;
+				break;
+			case 4:
+				weight *= 4.612;
+				finished = true;
+				break;
+			default:
+				System.out.println("That is not a valid number!");
+			}
+		}
+		System.out.println("Your weight on " + planets[selection - 1] + " is " + weight);
+
+	}
+
+}
