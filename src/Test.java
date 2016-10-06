6a3264ce546ee08351832a613bd01bd54162b852
Ryan McGee <ramtech51@comcast.net>
2016-10-06 10:09:34 -0400

    just changed Test a bit

--- src/Test.java
+++ src/Test.java
@@ -2,12 +2,6 @@
 public class Test {
 
 	public static void main(String[] args) {
-		int j,k;
-		int count = 0;
-		for(j = 0; j < 4; j++)
-			for(k = 0; k < 10; k++)
-				count++;
-		System.out.println(count--);
-		System.out.println(count);
+		System.out.println("Hello, World!".substring(2,3));
 	}
 }
