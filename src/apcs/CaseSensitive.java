48acbde84c113b3b9bafa9f35d0c76b73fca8228
Ryan McGee <Ryan McGee@MV0411095262J10.mv.scps.k12.va.us>
2016-10-11 14:13:35 -0400

    Finished the CaseSensitive project

--- src/apcs/CaseSensitive.java
+++ src/apcs/CaseSensitive.java
@@ -1,5 +1,6 @@
 package apcs;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CaseSensitive {
@@ -11,18 +12,60 @@ public class CaseSensitive {
 		Scanner input = new Scanner(System.in);
 		String str = input.nextLine();
 		input.close();
-		String lowercase = "qwertyuiopasdfghjklzxcvbnm";
-		String uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
-		int numOfLowercase = 0;
-		int numOfUppercase = 0;
-		for (int i = 0; i < str.length(); i++)
-			if (lowercase.contains(str.substring(i, i + 1)))
-				numOfLowercase++;
-		for (int i = 0; i < str.length(); i++)
-			if (uppercase.contains(str.substring(i, i + 1)))
-				numOfUppercase++;
-		System.out.println(str + " contains " + numOfLowercase + " lowercase letters and " + numOfUppercase
-				+ " uppercase letters.");
+		String punctuation = "`1234567890-=+_)(*&^%$#@!~,./?><;:][}{|";
+		String uppercaseVowels = "AEIOU";
+
+		/*
+		 * 0-4 = a,e,i,o,u, respectively
+		 */
+		int[] vowels = new int[5];
+		int numOfCons = 0;
+		int numOfPunc = 0;
+		int numOfSpaces = 0;
+		ArrayList<Character> otherChars = new ArrayList<Character>();
+		for (int i = 0; i < str.length(); i++) {
+			switch (str.charAt(i)) {
+			case 'a':
+				vowels[0]++;
+				break;
+			case 'e':
+				vowels[1]++;
+				break;
+			case 'i':
+				vowels[2]++;
+				break;
+			case 'o':
+				vowels[3]++;
+				break;
+			case 'u':
+				vowels[4]++;
+				break;
+			default:
+				otherChars.add(str.charAt(i));
+			}
+		}
+		while (otherChars.size() > 0) {
+			if (uppercaseVowels.contains(otherChars.get(otherChars.size() - 1).toString())) {
+				otherChars.remove(otherChars.size() - 1);
+			} else if (punctuation.contains(otherChars.get(otherChars.size() - 1).toString())) {
+				otherChars.remove(otherChars.size() - 1);
+				numOfPunc++;
+			} else if (otherChars.get(otherChars.size() - 1) == ' ') {
+				otherChars.remove(otherChars.size() - 1);
+				numOfSpaces++;
+			} else {
+				otherChars.remove(otherChars.size() - 1);
+				numOfCons++;
+			}
+		}
+		System.out.println("Number of lowercase vowels: \n\ta: " + vowels[0] 
+				+ "\n\te: " + vowels[1] 
+				+ "\n\ti: "	+ vowels[2] 
+				+ "\n\to: " + vowels[3] 
+				+ "\n\tu: " + vowels[4]);
+		System.out.println("Number of Consonants: " + numOfCons);
+		System.out.println("Number of Punctuation Marks: " + numOfPunc);
+		System.out.println("Number of Spaces: " + numOfSpaces);
 	}
 
 }
