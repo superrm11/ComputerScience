7173b3298070ff24018e15f929daa04170fd932c
Ryan McGee <Ryan McGee@MV0411095262J10.mv.scps.k12.va.us>
2016-11-02 10:38:23 -0400

    Finished the IBCS project

--- src/ibcs/CreditCardValidator.java
+++ src/ibcs/CreditCardValidator.java
@@ -1,37 +1,78 @@
 package ibcs;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class CreditCardValidator {
+/**
+ * 
+ * @author Ryan McGee
+ *
+ */
+public class CreditCardValidator
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please input a credit card number to validate: ");
+		System.out.println("Please input a 16 digit credit card number to validate: ");
 		long crNum = input.nextLong();
-		ArrayList<Integer> digits = getEachDigit(crNum);
+		input.close();
+		int[] digits = getEachDigit16(crNum);
 		int sum = 0;
-		for(int i = 0; i < digits.size(); i++){
-			sum += digits.get(i);
+		for (int i = 1; i < digits.length; i += 2)
+		{
+			System.out.print(digits[i]);
+			digits[i] = getEachDigit2(digits[i] * 2)[0] + getEachDigit2(digits[i] * 2)[1];
+			System.out.println(" Turns into: " + digits[i]);
 		}
-		if(sum % 10 == 0){
+		for (int i = 0; i < digits.length; i++)
+		{
+			sum += digits[i];
+		}
+		System.out.println(sum);
+		if (sum % 10 == 0)
+		{
 			System.out.println("This IS a valid credit card number.");
-		}else{
+		} else
+		{
 			System.out.println("This is NOT a valid credit card number.");
 		}
 
 	}
 
-	private static ArrayList<Integer> getEachDigit(long i) {
-		ArrayList<Integer> digit = new ArrayList<Integer>();
-		while (i > 0) {
+	private static int[] getEachDigit16(long i)
+	{
+		int[] digit = new int[16];
+		int digitNum = 0;
+		while (i > 0)
+		{
 			int currentDigit = 0;
-			while (i % 10 != 0) {
+			while (i % 10 != 0)
+			{
 				currentDigit++;
 				i--;
 			}
 			i /= 10;
-			digit.add(currentDigit);
+			digit[digitNum] = currentDigit;
+			digitNum++;
+		}
+		return digit;
+	}
+
+	private static int[] getEachDigit2(int i)
+	{
+		int[] digit = new int[2];
+		int digitNum = 0;
+		while (i > 0)
+		{
+			int currentDigit = 0;
+			while (i % 10 != 0)
+			{
+				currentDigit++;
+				i--;
+			}
+			i /= 10;
+			digit[digitNum] = currentDigit;
+			digitNum++;
 		}
 		return digit;
 	}
