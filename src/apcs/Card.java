90ff1519b58483f10153e34dbe9cece625d74e46
Ryan McGee <Ryan McGee@MV0411095262J10.mv.scps.k12.va.us>
2016-11-04 14:13:24 -0400

    Finished 1 more APCS project

---
+++ src/apcs/Card.java
@@ -0,0 +1,83 @@
+package apcs;
+
+public class Card
+{
+	public enum Suit
+	{
+		hearts, diamonds, clubs, spades, undefined
+	}
+
+	private Suit suit = Suit.undefined;
+
+	public Suit getSuit()
+	{
+		return suit;
+	}
+
+	public enum FaceValue
+	{
+		jack, queen, king, ace, number, undefined
+	}
+
+	private FaceValue faceValue = FaceValue.undefined;
+
+	private int faceValueInt = 0;
+
+	public FaceValue getFaceValue()
+	{
+		return faceValue;
+	}
+
+	public int getFaceValueInt()
+	{
+		return faceValueInt;
+	}
+
+	public void shuffle()
+	{
+		int i = (int) (Math.random() * 4);
+		switch (i)
+		{
+		case 0:
+			suit = Suit.clubs;
+			break;
+		case 1:
+			suit = Suit.diamonds;
+			break;
+		case 2:
+			suit = Suit.hearts;
+			break;
+		case 3:
+			suit = Suit.spades;
+			break;
+		default:
+			suit = Suit.undefined;
+		}
+
+		i = (int) (Math.random() * 14);
+		switch (i)
+		{
+		case 10:
+			faceValue = FaceValue.jack;
+			faceValueInt = 0;
+			break;
+		case 11:
+			faceValue = FaceValue.queen;
+			faceValueInt = 0;
+			break;
+		case 12:
+			faceValue = FaceValue.king;
+			faceValueInt = 0;
+			break;
+		case 13:
+			faceValue = FaceValue.ace;
+			faceValueInt = 0;
+			break;
+		default:
+			faceValue = FaceValue.number;
+			faceValueInt = i + 1;
+
+		}
+	}
+
+}
---
+++ src/apcs/Deal20Cards.java
@@ -0,0 +1,21 @@
+package apcs;
+
+public class Deal20Cards
+{
+
+	public static void main(String[] args)
+	{
+		Card[] c = new Card[20];
+		for(int i = 0; i < 20; i++){
+			c[i] = new Card();
+			c[i].shuffle();
+			if(c[i].getFaceValue() == Card.FaceValue.number){
+				System.out.println( i + 1 + "] You drew a " + c[i].getFaceValueInt() + " of " + c[i].getSuit());
+			}
+			else{
+				System.out.println(i + 1 + "] You drew a " + c[i].getFaceValue() + " of " + c[i].getSuit());
+			}
+		}
+	}
+
+}
