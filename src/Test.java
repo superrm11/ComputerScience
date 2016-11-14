857de48f9566d3c0e57db7e2fcb9b7825a1408d3
Ryan McGee <ramtech51@comcast.net>
2016-11-14 09:17:01 -0500

    Only changed Test

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,11 @@
 
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, World!".substring(2,3));
+	public static void main(String[] args)
+	{
+		System.out.println(translate(1));
+	}
+	public static double translate(int i){
+		return (double) i;
 	}
 }
