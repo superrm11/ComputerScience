62773830b97dd1fb53d8451529994c3fef8462f9
Ryan McGee <ramtech51@comcast.net>
2017-01-05 19:12:40 -0500

    just to merge... no biggie

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,38 @@
 
-public class Test {
+public class Test
+{
 
 	public static void main(String[] args)
 	{
-		System.out.println(translate(1));
+		int[][] array = (translate(new int[][]
+		{
+				{ 1, 2, 3, 4, 5 },
+				{ 6, 7, 8, 9, 0 } }));
+		
+		int[] a = {5,6}, b = {6,7};
+		
+		
+
+		for (int[] i : array)
+		{
+			for (int k : i)
+				System.out.println(k);
+			System.out.println("");
+		}
 	}
-	public static double translate(int i){
-		return (double) i;
+
+	public static int[][] translate(int[][] array)
+	{
+		int numOfRows = array.length;
+		int numOfColumns = array[0].length;
+		int[][] newNums = new int[numOfColumns][numOfRows];
+		for (int i = 0; i < array.length; i++)
+		{
+			for (int k = 0; k < array[i].length; k++)
+			{
+				newNums[k][i] = array[i][k];
+			}
+		}
+		return newNums;
 	}
 }
