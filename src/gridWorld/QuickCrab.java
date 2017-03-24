b67504730d73f5af363c40c6c73af0efd9034a4f
mcgeeryana <mcgeeryana@MV0411095262J10.mv.scps.k12.va.us>
2017-03-24 10:38:49 -0400

    worked on some apcs stuffs

--- src/gridWorld/QuickCrab.java
+++ src/gridWorld/QuickCrab.java
@@ -10,6 +10,20 @@ public class QuickCrab extends CrabCritter
 		Location possibleLocation1 = this.getLocation().getAdjacentLocation(270).getAdjacentLocation(270),
 				possibleLocation2 = this.getLocation().getAdjacentLocation(90).getAdjacentLocation(90);
 
+		if ((this.getGrid().isValid(possibleLocation2) && this.getGrid().isValid(possibleLocation1))
+				&& (this.getGrid().get(possibleLocation1) == null && this.getGrid().get(possibleLocation2) == null))
+		{
+			this.moveTo(((int)(Math.random() * 2) == 0) ? possibleLocation1 : possibleLocation2);
+		} else if ((this.getGrid().isValid(possibleLocation2) && this.getGrid().get(possibleLocation2) == null)
+				|| (this.getGrid().isValid(possibleLocation1) && this.getGrid().get(possibleLocation1) == null))
+		{
+			this.moveTo((this.getGrid().isValid(possibleLocation2) && this.getGrid().get(possibleLocation2) == null)
+					? possibleLocation2 : possibleLocation1);
+		}else
+		{
+			super.act();
+		}
+
 	}
 
 	public static void main(String[] args)
